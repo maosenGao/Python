6de99da8c61133ed7849fba57b6b14e75b5c9839
maosenGao <gaomaosen@outlook.com>
2019-11-12 19:33:52 +0800

    Update all_combinations.py

--- backtracking/all_combinations.py
+++ backtracking/all_combinations.py
@@ -3,7 +3,14 @@
 """
 	In this problem, we want to determine all possible combinations of k
 	numbers out of 1 ... n. We use backtracking to solve this problem.
+
+
 	Time complexity: O(C(n,k)) which is O(n choose k) = O((n!/(k! * (n - k)!)))
+
+    在这个问题中，我们想要确定所有k个数字的可能组合。
+    我们用回溯法来解决这个问题。
+    
+    时间复杂度:o(c(n,k))即o(n选k) =o((n!/(k!)* (n - k) !)))
 """
 
 
